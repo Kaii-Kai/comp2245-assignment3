26cd31c71e853c0fc4d193b91c2831b2758a766f
Kyra Lee <500001736@my.fiveislands.uwi.edu>
2024-10-27 21:17:58 -0400

    Add an X or O to a square when clicked

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,3 +3,18 @@ document.addEventListener("DOMContentLoaded", () => {
 	let squares = document.querySelectorAll("#board div");
 	squares.foreach(square => square.classList.add("square"));
 });
+
+let player = "X";
+const gameSate = Array(9).fill(null);
+
+document.querySelectorAll("#board div").foreach((square, index) => {
+	square.addEventListener("click", () => {
+		if (!gameSate[index]) {
+			gameState[index] = player;
+			square.textContent = player;
+			square.classList.add(player);
+			
+			player = player === "X" ? "O" : "X";
+		}
+	});
+});
